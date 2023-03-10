7228b53a3fcd9e01f1da607357f6d2df9955a996
Gurkan <40955470+GHasanKaraman@users.noreply.github.com>
2023-03-10 13:53:21 -0500

    vendor backend is done

--- backend/routes/vendor.js
+++ backend/routes/vendor.js
@@ -1,54 +1,53 @@
 const express = require("express");
 const router = express.Router();
 
-const technicianModel = require("../models/technician");
+const vendorModel = require("../models/vendor");
 
-router.post("/technician", async (req, res) => {
-  const techs = await technicianModel.find({});
-  if (techs) {
+router.post("/vendor", async (req, res) => {
+  const vendors = await vendorModel.find({});
+  if (vendors) {
     res.json({
       status: "success",
-      records: { ...techs },
+      records: { ...vendors },
     });
-    console.log("Retrieved technicians!");
+    console.log("Retrieved vendors!");
   } else {
     res.json({ status: "failed" });
-    console.log("\x1b[31m%s\x1b[0m", "Didn't retrieve technicians!");
+    console.log("\x1b[31m%s\x1b[0m", "Didn't retrieve vendors!");
   }
 });
 
-router.post("/technician/add", async (req, res) => {
+router.post("/vendor/add", async (req, res) => {
   try {
-    const { name } = req.body;
-    const result = await technicianModel.create({
-      name: name.toUpperCase(),
+    const { vendor } = req.body;
+    const result = await vendorModel.create({
+      vendor: vendor.toUpperCase(),
     });
     res.json(
       result._id
         ? { result: "success", resultData: result }
         : { result: "failed" }
     );
-    console.log("Technician " + name + " is written in the database");
+    console.log("Vendor " + vendor + " is written in the database");
   } catch (e) {
     console.log(e);
     res.json({ error: e });
   }
 });
 
-router.post("/technician/delete", async (req, res) => {
+router.post("/vendor/delete", async (req, res) => {
   try {
     const id = req.body.id;
     if (id) {
-      const result = await technicianModel.deleteOne({ _id: id });
+      const result = await vendorModel.deleteOne({ _id: id });
       if (result.deletedCount != 0) {
         res.json({ status: "success" });
-        console.log("\x1b[32m%s\x1b[0m", "Technician deleted!");
+        console.log("\x1b[32m%s\x1b[0m", "Vendor deleted!");
       } else {
         res.json({ status: "failed" });
         console.log(
           "\x1b[31m%s\x1b[0m",
-          "Didn't find the technician for deletion. ID of the technician is " +
-            id
+          "Didn't find the vendor for deletion. ID of the vendor is " + id
         );
       }
     } else {
@@ -61,25 +60,25 @@ router.post("/technician/delete", async (req, res) => {
   }
 });
 
-router.post("/technician/update", async (req, res) => {
+router.post("/vendor/update", async (req, res) => {
   try {
-    const { _id, name } = req.body;
+    const { _id, vendor } = req.body;
 
     if (_id) {
-      const result = await technicianModel.updateOne(
+      const result = await vendorModel.updateOne(
         { _id: _id },
-        { name: name.toUpperCase() }
+        { vendor: vendor.toUpperCase() }
       );
       if (result.modifiedCount != 0) {
-        console.log("\x1b[32m%s\x1b[0m", "Technician updated!");
+        console.log("\x1b[32m%s\x1b[0m", "Vendor updated!");
         res.json({ status: "success" });
       } else {
         res.json({ status: "failed" });
 
         console.log(
           "\x1b[31m%s\x1b[0m",
-          req.user[0].name +
-            " didn't update the technician. ID of the technician is " +
+          req.user[0].vendor +
+            " didn't update the vendor. ID of the vendor is " +
             _id
         );
       }
